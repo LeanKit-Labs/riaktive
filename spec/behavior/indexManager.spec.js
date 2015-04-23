04356e4066c1944019ad16b421554d46a0a91715
JohnDMathis <john.mathis@leankit.com>
2015-04-23 10:04:23 -0500

    Completed specs for indexManager

--- spec/behavior/indexManager.spec.js
+++ spec/behavior/indexManager.spec.js
@@ -23,11 +23,46 @@ describe( 'indexManager', function() {
 	describe( 'when creating an index', function() {
 		var indexMgr;
 
-		describe( 'and the index already exists', function() {
-			var riak;
+		describe( 'and it is new', function() {
+			var riak, timeoutVal;
 			before( function() {
 				riak = {
-					yzGetIndex: noOp
+					yzGetIndex: noOp,
+					yzPutIndex: noOp
+				};
+				sinon.stub( riak, 'yzGetIndex', function() {
+					return when( {} );
+				} );
+				sinon.stub( riak, 'yzPutIndex', function() {
+					return when();
+				} );
+				indexMgr = new IndexMgr( riak );
+
+				return indexMgr.create( 'index1', 'foo' )
+					.then( function( tm ) {
+						timeoutVal = tm;
+					} );
+			} );
+
+			it( 'checks for an existing index', function() {
+				riak.yzGetIndex.withArgs( { name: 'index1' } ).calledOnce.should.be.true;
+			} );
+
+			it( 'creates a new index', function() {
+				riak.yzPutIndex.withArgs( { index: { name: 'index1', schema: 'foo' } } )
+					.calledOnce.should.be.true;
+			} );
+			it( 'returns a timeout value of 10 seconds', function() {
+				timeoutVal.should.equal( 10000 );
+			} );
+		} );
+
+		describe( 'and the index already exists', function() {
+			var riak, timeoutVal;
+			before( function() {
+				riak = {
+					yzGetIndex: noOp,
+					yzPutIndex: noOp
 				};
 				sinon.stub( riak, 'yzGetIndex', function() {
 					var fake = {
@@ -37,14 +72,30 @@ describe( 'indexManager', function() {
 					};
 					return when( fake );
 				} );
+				sinon.stub( riak, 'yzPutIndex', function() {
+					return when();
+				} );
+
 				indexMgr = new IndexMgr( riak );
-				debugger;
+
 				return indexMgr.create( 'index1', 'foo' )
-					.then( function() {
-						return indexMgr.create( 'index1', 'foo' );
+					.then( function( val ) {
+						return indexMgr.create( 'index1', 'foo' )
+							.then( function( tm ) {
+								timeoutVal = tm;
+							} );
 					} );
 			} );
-			it( 'returns the existing index', function() {} );
+			it( 'checks for an existing index', function() {
+				riak.yzGetIndex.calledOnce.should.be.true;
+			} );
+
+			it( 'does not create a new index', function() {
+				riak.yzPutIndex.calledOnce.should.be.true;
+			} );
+			it( 'returns a timeout value of 0 seconds', function() {
+				timeoutVal.should.equal( 0 );
+			} );
 		} );
 	} );
 } );
