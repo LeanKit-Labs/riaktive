35aed397c097e274801ae0299afd4aec02fc3473
Brian Edgerton <brian.edgerton@leankit.com>
2015-10-08 10:25:10 -0500

    Updated biggulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,3 @@
-var gulp = require( 'gulp' );
-var bg = require( 'biggulp' )( gulp );
+var gulp = require( "gulp" );
 
-gulp.task( 'coverage', bg.withCoverage() );
-
-gulp.task( 'coverage-watch', function() {
-	bg.watch( [ 'coverage' ] );
-} );
-
-gulp.task( 'show-coverage', bg.showCoverage() );
-
-gulp.task( 'continuous-specs', function() {
-	return bg.test();
-} );
-
-gulp.task( 'specs-watch', function() {
-	bg.watch( [ 'continuous-specs' ] );
-} );
-
-gulp.task( 'test-and-exit', function() {
-	return bg.testOnce();
-} );
-
-gulp.task( 'default', [ 'coverage', 'coverage-watch' ], function() {} );
-gulp.task( 'specs', [ 'continuous-specs', 'specs-watch' ], function() {} );
-gulp.task( 'build', [ 'test-and-exit' ] );
+require( "biggulp/common-gulp" )( gulp );
